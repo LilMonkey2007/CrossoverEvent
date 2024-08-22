85c7e22ab64dab2203c6a0478ae2a66385b1d65a
LilMonkey2007 <wj-wolf@hotmail.com>
2024-08-22 16:04:22 +0800

    split app js to 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import logo from './logo.svg'; 
-import { useState } from 'react';
+
+import { AboutPage } from './useStates/AboutPage';
+import { MyButton } from './useStates/MyButton';
 
 const products = [
     { title: 'Cabbage', isFruit: false, id: 1 },
@@ -8,24 +10,6 @@ const products = [
     { title: 'Apple', isFruit: true, id: 3 },
 ];
 
-function MyButton() {
-    const [count, setCount] = useState(0);
-    function handleClick() {
-        alert('You clicked me!');
-        setCount(count + 1);
-    }
-    return (
-        <button onClick={handleClick}>Clicked {count} times</button>
-    );
-}
-function AboutPage() {
-    return (
-        <>
-            <h1>About</h1>
-            <p>Hello there.<br />How do you do?</p>
-        </>
-    );
-}
 
 export default function MyApp() {
     const listItems = products.map(product =>
@@ -41,11 +25,11 @@ export default function MyApp() {
     return (
         <div>
             <img src={logo} className="App-logo" alt="logo" />
-            <AboutPage></AboutPage>
+            <AboutPage/>
             <h1>
                 Welcome to my app
             </h1>
-            <MyButton></MyButton>
+            <MyButton/>
 
             <ul>{listItems}</ul>
         </div>
---
+++ src/useStates/MyButton.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export function MyButton() {
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        alert('You clicked me!');
+        setCount(count + 1);
+    }
+    return (
+        <button onClick={handleClick}>Clicked {count} times</button>
+    );
+}
